1ce272355ec088cfdc4f3a937a964b818f102216
GForss <konst665@gmail.com>
2022-12-06 17:43:18 +0300

    H4t1

---
+++ H4/Task 25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int numbers(int num1, int num2){
+
+    int itog = num1;
+
+    for (int i = 1; i < num2; i++) {
+        itog *= num1; 
+    }
+    return itog;
+}
+
+WriteLine(numbers(int.Parse(ReadLine()), int.Parse(ReadLine())));
