aac5edd8fa8abec61ecbd876f627f93e1b96b0ac
GForss <konst665@gmail.com>
2022-12-07 12:23:26 +0300

    H4

---
+++ H4/Task 27/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int summ(int num) {
+
+    int itog = 0;
+
+    while (num > 0) {
+        itog += num % 10;
+        num = num / 10;
+    }
+    return itog;
+}
+ WriteLine(summ(int.Parse(ReadLine())));
---
+++ H4/Task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] array(int size) {
+
+    int[] bro = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        bro[i] = new Random().Next(0, 100);
+    }
+    return bro;
+}   
+
+WriteLine(String.Join(",", array(8)));
