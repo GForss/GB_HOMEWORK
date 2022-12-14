ff67b4bb926255e29f0e6814d61c09233b13ffa8
GForss <konst665@gmail.com>
2022-12-14 12:10:19 +0300

    H6

---
+++ Н6/Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] array = Hreh(ReadLine());
+
+int[] Hreh(string massiv)
+{
+    string[] arr1 = massiv.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arr1.Length];
+
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        result[i] = int.Parse(arr1[i]);
+    }
+    return result;
+}
+
+WriteLine(String.Join(",", array));
+
+int number = Summ(array);
+
+int Summ(int[] arr)
+{
+    int itog = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            itog++;
+        }
+    }
+    return itog;
+}
+
+WriteLine(number);
---
+++ Н6/Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите b1: ");
+double b1 = int.Parse(ReadLine());
+WriteLine("Введите b2: ");
+double b2 = int.Parse(ReadLine());
+WriteLine("Введите k1: ");
+double k1 = int.Parse(ReadLine());
+WriteLine("Введите k2: ");
+double k2 = int.Parse(ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+WriteLine($"X: {x}, Y: {y}");
